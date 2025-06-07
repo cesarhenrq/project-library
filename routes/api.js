dcd63b5139f373347b97629aec015c535f99bed1
Cesar Henrique <cesaarhvb@gmail.com>
2025-06-07 11:55:58 -0300

    feat(api): add book model and update API routes for book management
    - Introduced a new Book model using Mongoose to represent book data.
    - Updated API routes to handle GET requests for retrieving books and added structure for POST and DELETE operations.
    - Enhanced package.json with a test script for running Mocha tests.
    - Modified functional tests to include setup for book data and assertions for API responses.

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,56 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
+
+const { Book } = require("../models");
 
 module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(async function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      const books = await Book.find();
+
+      const booksResponse = books.map((book) => ({
+        _id: book._id,
+        title: book.title,
+        commentcount: book.comments.length,
+      }));
+
+      res.json(booksResponse);
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
